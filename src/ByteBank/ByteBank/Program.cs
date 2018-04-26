f7b98948f9862013a3d49bf1ce84983f4d914201
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-26 17:40:46 -0300

    Video 1.2

---
+++ src/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario("434.568.190-20");
+
+            carlos.Nome = "Carlos";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.ReadLine();
+        }
+    }
+}

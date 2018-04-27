62b3b6c07589ac01f35649434e84eedeadb5b554
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-27 17:20:12 -0300

    Video 3.1 (regravei)

--- src/ByteBank/ByteBank/Program.cs
+++ src/ByteBank/ByteBank/Program.cs
@@ -13,20 +13,24 @@ namespace ByteBank
         {
             GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
-            Funcionario carlos = new Funcionario("454.789.157-3");
+            Funcionario carlos = new Funcionario("546.879.157-20");
             carlos.Nome = "Carlos";
             carlos.Salario = 2000;
 
+            Console.WriteLine(Funcionario.TotalDeFuncionarios);
+
             gerenciador.Registrar(carlos);
-            
-            Diretor roberta = new Diretor("542.587.289-3");
+
+            Diretor roberta = new Diretor("454.658.148-3");
             roberta.Nome = "Roberta";
             roberta.Salario = 5000;
 
+            Console.WriteLine(Funcionario.TotalDeFuncionarios);
+
             Funcionario robertaTeste = roberta;
 
-            Console.WriteLine("CPF de uma referencia de Diretor: " + roberta.CPF);
-            Console.WriteLine("CPF de uma referencia de Funcionario: " + robertaTeste.CPF);
+            Console.WriteLine("Bonificacao de uma referencia de Diretor: " + roberta.GetBonificacao());
+            Console.WriteLine("Bonificacao de uma referencia de Funcionario: " + robertaTeste.GetBonificacao());
 
 
             gerenciador.Registrar(roberta);
